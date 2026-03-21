c1a0702a14977454dcd81964091f428c1b7c8d13
RMaggi0303 <CSMRMAGG@ljmu.ac.uk>
2026-03-21 19:23:14 +0000

    updated visualisation_riccardo.py

--- src/visualisations/riccardo/visualisation_riccardo.py
+++ src/visualisations/riccardo/visualisation_riccardo.py
@@ -1,6 +1,15 @@
 from classes.file_io import FileIO
+from utility.row_transformations import string_time_to_minutes
 import matplotlib.pyplot as mpl
+from matplotlib.ticker import FuncFormatter
+import matplotlib.dates as mdates
 import pandas as pd
+import matplotlib.ticker as ticker
+
+def minutes_to_military(total_minutes, tick_number):
+    hours = int(total_minutes // 60)
+    minutes = int(total_minutes % 60)
+    return f"{hours:02d}:{minutes:02d}"
 
 def daylight_hours():
     # Get dataset
@@ -19,15 +28,26 @@ def daylight_hours():
         else:
             print("Country not found, have you typed the name correctly?")
 
-    sunset_time = df["sunset"]
-    sunrise_time = df["sunrise"]
-    light_hours = sunset_time - sunrise_time   #TODO convert to military time
-    print(light_hours)
-
     # process data per name of country (filter)
     # TODO
-    df = df[df["country"] == country]
-    df = df.loc[(df["last_updated"] > "2025-1-1")]
+    filtered_df = df[df["country"] == country]
+    filtered_df["last_updated"] = pd.to_datetime(filtered_df["last_updated"])
+    filtered_df = filtered_df.loc[(filtered_df["last_updated"] > "2025-1-1")]
+    filtered_df = filtered_df[filtered_df["last_updated"].dt.day == 1]
+
+    daylight_minutes_list = []
+    dates = []
+
+    for _, row in filtered_df.iterrows():                             #ask matthieu
+        sunset_minutes = string_time_to_minutes(row["sunset"])
+        sunrise_minutes = string_time_to_minutes(row["sunrise"])
+        
+        daylight_minutes = sunset_minutes - sunrise_minutes    #TODO convert to military time to display in labels#
+        
+        daylight_minutes_list.append(daylight_minutes)
+        dates.append(row["last_updated"])
+        
+    print(dates)
 
     # Ask for sorting
     # TODO
@@ -42,5 +62,14 @@ def daylight_hours():
     # Visualise via matplotlib
     # TODO filter or group data, else visualization will slow program
     fig, ax = mpl.subplots()
-    ax.plot(df["last_updated"], df["sunrise"], df["sunset"])
+    ax.plot(dates, daylight_minutes_list)
+
+    ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+    ax.yaxis.set_major_formatter(mpl.FuncFormatter(minutes_to_military))
+    
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Daylight Hours")
+    ax.set_title(f"Daylight Hours in {country}")
+
+    mpl.xticks(dates, rotation=45)
     mpl.show()
