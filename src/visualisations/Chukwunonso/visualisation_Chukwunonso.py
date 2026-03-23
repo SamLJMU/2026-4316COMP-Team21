670199a2454312972e5e0bfb94ea9c7ca031cd68
Chuk404 <turket2434@gmail.com>
2026-03-23 20:25:16 +0000

    Update visualisation_Chukwunonso.py

---
+++ src/visualisations/Chukwunonso/visualisation_Chukwunonso.py
@@ -0,0 +1,23 @@
+from classes.file_io import FileIO
+import matplotlib.pyplot as mpl
+import pandas as pd
+from input import getCountryInput
+
+
+def temperature_celsius_to_feels_like_celsius():
+    # Filter Input
+    df = FileIO.dataset_df[
+        ["temperature_celsius", "feels_like_celsius", "last_updated ", "country"]
+    ]
+    # Filter Input
+    country = getCountryInput(
+        "Enter a country name: ",
+        df["country"].to_list(),
+        "Country not found. Try again",)
+
+ # Process data per filter
+df = df [df["country"] == country]
+df = df.loc[(df["last_updated_date_time"] >= "2024-01-01")]
+
+
+    
