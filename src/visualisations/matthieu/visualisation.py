3c85f29118c49482b61d6ac61a0edc75c1cc1ab3
Matthieu-MG <matthieu.loic@icloud.com>
2026-03-28 11:20:37 +0000

    Worked on my visualisation

--- src/visualisations/matthieu/visualisation.py
+++ src/visualisations/matthieu/visualisation.py
@@ -1,11 +1,11 @@
 from classes.file_io import FileIO
 import matplotlib.pyplot as mpl
-import pandas as pd
+import numpy as np
 from input import input_country, input_timeframe
 
 def air_quality_over_time():
     # Get dataset
-    df = FileIO.dataset_df[["country", "last_updated_date_time", "air_quality_PM2.5"]]
+    df = FileIO.dataset_df[["country", "last_updated_date_time", "air_quality_PM2.5", "temperature_celsius"]]
 
     # Filter Input
     country = input_country("Enter a country name: ", "Country not found. Try again")
@@ -29,5 +29,12 @@ def air_quality_over_time():
     # Visualise via matplotlib
     fig, ax = mpl.subplots()
     # ax.plot(df.index, df["air_quality_PM2.5"])
-    ax.plot(df["last_updated_date_time"], df["air_quality_PM2.5"])
+    #ax.plot(df["last_updated_date_time"], df["air_quality_PM2.5"])
+    #ax.plot(df["last_updated_date_time"], df["temperature_celsius"])
+    ax.scatter(df["temperature_celsius"], df["air_quality_PM2.5"])
+    
+    x = df["temperature_celsius"]
+    y = df["air_quality_PM2.5"]
+    ax.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+
     mpl.show()
