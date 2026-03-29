4811a2aeb3f57dc055ef9a342278f576c22c18ed
Chuk404 <turket2434@gmail.com>
2026-03-29 10:49:58 +0100

    Added Matthieu time function to my visualisation

--- src/visualisations/Chukwunonso/visualisation_Chukwunonso.py
+++ src/visualisations/Chukwunonso/visualisation_Chukwunonso.py
@@ -1,7 +1,8 @@
 from classes.file_io import FileIO
 import matplotlib.pyplot as mpl
 import pandas as pd
-from input import input_country
+from input import input_country, input_timeframe
+
 
 def temperature_celsius_to_feels_like_celsius():
     # Filter Input
@@ -15,15 +16,15 @@ def temperature_celsius_to_feels_like_celsius():
     ]
     # Get country from user
     country = input_country()
-
+    timeframe = input_timeframe("Desired timeframe: ")
     # Process data per filter
     df = df[df["country"] == country]
     df["last_updated_date_time"] = pd.to_datetime(df["last_updated_date_time"])
-    df = df.loc[df["last_updated_date_time"] >= pd.Timestamp("2024-01-01")]
+    df = df.loc[(df["last_updated_date_time"] >= timeframe[0]) & (df["last_updated_date_time"] <= timeframe[1])]
 
     # Visualise via matplotlib
     fig, ax = mpl.subplots()
-    ax.plot( df["last_updated_date_time"], df["temperature_celsius"], label="Temperature" )
+    ax.plot(df["last_updated_date_time"], df["temperature_celsius"], label="Temperature")
     ax.plot(df["last_updated_date_time"], df["feels_like_celsius"], label="Feels Like")
     ax.legend()
     mpl.show()
--- src/visualisations/Chukwunonso/visualisation_Chukwunonso2.py
+++ src/visualisations/Chukwunonso/visualisation_Chukwunonso2.py
@@ -1,7 +1,7 @@
 from classes.file_io import FileIO
 import matplotlib.pyplot as mpl
 import pandas as pd
-from input import input_country
+from input import input_country, input_timeframe
 
 
 def Wind_mph_to_gust_mph():
@@ -12,16 +12,16 @@ def Wind_mph_to_gust_mph():
 
     # Get country from user
     country = input_country()
+    timeframe = input_timeframe("Desired timeframe:")
 
     # Process data per filter
     df = df[df["country"] == country]
     df["last_updated_date_time"] = pd.to_datetime(df["last_updated_date_time"])
-    df = df.loc[df["last_updated_date_time"] >= pd.Timestamp("2024-01-01")]
+    df = df.loc[(df["last_updated_date_time"] >= timeframe[0]) & (df["last_updated_date_time"] <= timeframe[1])]
 
     # Visualise via matplotlib
     fig, ax = mpl.subplots()
     ax.plot(df["last_updated_date_time"], df["wind_mph"], label="Wind Speed")
     ax.plot(df["last_updated_date_time"], df["gust_mph"], label="Gust")
     ax.legend()
-
     mpl.show()
