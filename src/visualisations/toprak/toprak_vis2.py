89b38d0e045f23c020f9a4b8d9252c98608cc028
TerraWispr <toprakceceli@gmail.com>
2026-04-24 03:00:39 +0100

    Added my visualisations

--- src/visualisations/toprak/toprak_vis2.py
+++ src/visualisations/toprak/toprak_vis2.py
@@ -4,9 +4,17 @@ from classes.file_io import FileIO
 from input import input_country, input_timeframe
 
 
-def humidity_vs_cloud_over_time():
+def cloud_vs_feels_like_temp_over_time():
     # Import relevant dataset columns
-    df = FileIO.dataset_df[["cloud", "humidity", "last_updated_date_time", "country"]]
+    df = FileIO.dataset_df[
+        [
+            "cloud",
+            "feels_like_celsius",
+            "feels_like_fahrenheit",
+            "last_updated_date_time",
+            "country",
+        ]
+    ]
 
     # Get user filter inputs
     country = input_country()
@@ -32,28 +40,53 @@ def humidity_vs_cloud_over_time():
         user_choice = input(
             f"Your chosen timeframe is {days_diff} days. Would you like to view monthly averages instead of daily? (Y/N): "
         ).upper()
-        if user_choice in ["Y", "YES"]:
+        if user_choice not in ["N", "NO"]:
+            if user_choice not in ["Y", "YES"]:
+                print(
+                    "Input not recognised. Going with the recommended option of monthly."
+                )
             df = (
                 df.set_index("last_updated_date_time")
                 .resample("ME")
                 .mean(numeric_only=True)
                 .reset_index()
             )
-        timescale = "Monthly"
+            timescale = "Monthly"
+
+    # Unit user choice
+    unit_choice = input(f"Would you prefer Celsius or Fahrenheit? (C/F): ").upper()
 
     # Graph visualistaion
     fig, ax = plt.subplots(figsize=(10, 8))
     ax.plot(
-        df["last_updated_date_time"], df["cloud"], label="Cloud Coverage", marker="o"
+        df["last_updated_date_time"],
+        df["cloud"],
+        label="Cloud Coverage (%)",
+        marker="o",
     )
-    ax.plot(df["last_updated_date_time"], df["humidity"], label="Humidity", marker="o")
+
+    if unit_choice not in ["F", "FAHRENHEIT"]:
+        if unit_choice not in ["C", "CELSIUS"]:
+            print("Input not recognised. Going with the recommended option of Celsius.")
+        ax.plot(
+            df["last_updated_date_time"],
+            df["feels_like_celsius"],
+            label="Feels-Like Temperature (°C)",
+            marker="o",
+        )
+    else:
+        ax.plot(
+            df["last_updated_date_time"],
+            df["feels_like_fahrenheit"],
+            label="Feels-Like Temperature (°F)",
+            marker="o",
+        )
 
     ax.set_xlabel("Date")
     ax.xaxis.grid(True)
-    ax.set_ylabel("%")
     ax.yaxis.grid(True)
 
-    ax.set_title(f"{timescale} Humidity vs. Cloud Coverage in {country}")
+    ax.set_title(f"{timescale} Cloud Coverage vs. Feels Like Temperature in {country}")
     ax.legend()
     fig.autofmt_xdate()
     plt.tight_layout()
